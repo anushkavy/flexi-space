4f3d806a08c3224e6df4173580465e51cf56c694
Anushka <anushkavarshney2708@gmail.com>
2024-08-24 11:19:36 +0530

    UI improvements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/" className="nav-btn">
+      <Link to="/" className="nav-item">
         <h1>FlexiSpace</h1>
       </Link>
       <div className="nav-content">
-        <Link to="/about" className="nav-btn">
+        <Link to="/about" className=" nav-item nav-btn">
           About
         </Link>
-        <Link to="/explore" className="nav-btn">
+        <Link to="/explore" className=" nav-item nav-btn">
           Explore Spaces
         </Link>
       </div>
